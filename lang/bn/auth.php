25dfdc044292a454a87593efcd439d019d0fd82c
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-07-15 12:47:29 +0600

    feat: validation translation complete

--- lang/bn/auth.php
+++ lang/bn/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'এই প্রমাণপ্রত্রাদি আমাদের রেকর্ডের সাথে মেলে না।',
+    'password' => 'প্রদত্ত পাসওয়ার্ডটি ভুল।',
+    'throttle' => 'অনেকগুলি লগইন প্রচেষ্টা। অনুগ্রহ করে :seconds সেকেন্ডের মধ্যে আবার চেষ্টা করুন।',
 
 ];
