578290a8bfcb9d19e6aee62ce0a698b5418e7608
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-10-03 00:18:25 +0600

    feat: Center soft Delete Complete

--- app/Http/Controllers/category/CategoryController.php
+++ app/Http/Controllers/category/CategoryController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\category;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\category\CategoryStoreRequest;
 use App\Models\category\Category;
+use App\Models\category\CategoryActionHistory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -65,6 +67,24 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        DB::transaction(function () use ($id) {
+            Category::find($id)->delete();
+            CategoryActionHistory::create([
+                "category_id"       => $id,
+                "author_id"         => auth()->id(),
+                "name"              => auth()->user()->name,
+                "image_uri"         => auth()->user()->image_uri,
+                "action_type"       => 'delete',
+                "action_details"    => [],
+            ]);
+        });
+
+        return response(
+            [
+                'success'   => true,
+                'message'   => __('customValidations.category.delete')
+            ],
+            200
+        );
     }
 }
