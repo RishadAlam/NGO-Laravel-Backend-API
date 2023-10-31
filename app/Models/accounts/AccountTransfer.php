1af60f362ab2e8de5a04edff1237d8391edd1cda
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-10-31 18:37:32 +0600

    feat: Account Transfer model & migration added

---
+++ app/Models/accounts/AccountTransfer.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models\accounts;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class AccountTransfer extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'tx_acc_id',
+        'rx_acc_id',
+        'amount',
+        'tx_prev_balance',
+        'tx_balance',
+        'rx_prev_balance',
+        'rx_balance',
+        'description',
+        'date',
+        'creator_id'
+    ];
+
+    /**
+     * Relationship belongs to User model
+     *
+     * @return response()
+     */
+    public function Author()
+    {
+        return $this->belongsTo(User::class, 'creator_id', 'id')->withTrashed();
+    }
+
+    /**
+     * Relationship belongs to Account model
+     *
+     * @return response()
+     */
+    public function TXAccount()
+    {
+        return $this->belongsTo(Account::class, 'tx_acc_id', 'id')->withTrashed();
+    }
+
+    /**
+     * Relationship belongs to Account model
+     *
+     * @return response()
+     */
+    public function RXAccount()
+    {
+        return $this->belongsTo(Account::class, 'rx_acc_id', 'id')->withTrashed();
+    }
+}
---
+++ database/migrations/2023_10_31_122629_create_account_transfers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('account_transfers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tx_acc_id')->constrained('accounts', 'id')->cascadeOnUpdate('cascade')->cascadeOnDelete('cascade')->comment('Transaction Sended Account');
+            $table->foreignId('rx_acc_id')->constrained('accounts', 'id')->cascadeOnUpdate('cascade')->cascadeOnDelete('cascade')->comment('Transaction Received Account');
+            $table->integer('amount');
+            $table->integer('tx_prev_balance');
+            $table->integer('tx_balance')->storedAs('tx_prev_balance - amount');
+            $table->integer('rx_prev_balance');
+            $table->integer('rx_balance')->storedAs('rx_prev_balance - amount');
+            $table->string('description');
+            $table->timestamp('date')->useCurrent();
+            $table->foreignId('creator_id')->nullable()->constrained('users', 'id')->cascadeOnUpdate('cascade')->nullOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('account_transfers');
+    }
+};
