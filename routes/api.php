b8cb3c76d7a27999dd7825ea2a66a419aedfcbfc
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-11-03 23:00:40 +0600

    chore: Api routes re-organized

--- routes/api.php
+++ routes/api.php
@@ -31,23 +31,23 @@ use App\Http\Controllers\accounts\AccountWithdrawalController;
  */
 Route::GET('/cache-clear', function () {
     Artisan::call('cache:clear');
-    return response("Cache is cleared");
+    return response("cache cleared");
 });
 Route::GET('/config-clear', function () {
     Artisan::call('config:clear');
-    return response("Cache is cleared");
+    return response("config cleared");
 });
 Route::GET('/route-clear', function () {
     Artisan::call('route:clear');
-    return response("Cache is cleared");
+    return response("route cleared");
 });
 Route::GET('/optimize-clear', function () {
     Artisan::call('optimize:clear');
-    return response("Cache is cleared");
+    return response("optimize cleared");
 });
 Route::GET('/storage-link', function () {
     Artisan::call('storage:link');
-    return response("Cache is cleared");
+    return response("storage Linked");
 });
 
 /**
@@ -65,11 +65,13 @@ Route::group(['middleware' => 'LangCheck'], function () {
      *
      * Here is where you can hit unAuthenticated routes
      */
-    Route::POST('/login', [AuthController::class, 'login']);
-    Route::POST('/forget-password', [AuthController::class, 'forget_password']);
-    Route::GET('/otp-resend/{id}', [AuthController::class, 'otp_resend']);
-    Route::POST('/account-verification', [AuthController::class, 'otp_verification']);
-    Route::PUT('/reset-password', [AuthController::class, 'reset_password']);
+    Route::controller(AuthController::class)->group(function () {
+        Route::POST('/login', 'login');
+        Route::POST('/forget-password', 'forget_password');
+        Route::GET('/otp-resend/{id}', 'otp_resend');
+        Route::POST('/account-verification', 'otp_verification');
+        Route::PUT('/reset-password', 'reset_password');
+    });
 });
 
 
@@ -88,12 +90,13 @@ Route::group(['middleware' => ['auth:sanctum', 'verified', 'LangCheck', 'activeU
      * Authorization Routes
      * -------------------------------------------------------------------------
      */
-    Route::GET('/authorization', [AuthController::class, 'authorization']);
-    Route::GET('/app-settings', [AppConfigController::class, 'index']);
-    Route::POST('/registration', [AuthController::class, 'registration']);
-    Route::POST('/logout', [AuthController::class, 'logout']);
-    Route::PUT('/change-password', [AuthController::class, 'change_password']);
-    Route::PUT('/profile-update', [AuthController::class, 'profile_update']);
+    Route::controller(AuthController::class)->group(function () {
+        Route::GET('/authorization', 'authorization');
+        Route::POST('/registration', 'registration');
+        Route::POST('/logout', 'logout');
+        Route::PUT('/change-password', 'change_password');
+        Route::PUT('/profile-update', 'profile_update');
+    });
 
     /**
      * -------------------------------------------------------------------------
@@ -107,23 +110,33 @@ Route::group(['middleware' => ['auth:sanctum', 'verified', 'LangCheck', 'activeU
     Route::GET('permissions/{id}', [PermissionController::class, 'index'])->name('permissions.index');                          // Permissions Index
     Route::GET('/fields/active', [FieldController::class, 'get_active_fields']);                                                // Get all active fields
     Route::GET('/categories/groups', [CategoryController::class, 'get_category_groups']);                                       // Get all Category Groups
-    Route::GET('/accounts/active', [AccountController::class, 'get_active_accounts']);                                          // Get all Category Groups
-    Route::GET('/accounts/transactions/{date_range?}/{account_id?}', [AccountController::class, 'get_all_transactions']);                                          // Get all Category Groups
-    Route::GET('/incomes/{date_range}', [IncomeController::class, 'index'])->name('incomes.index');                             // Get all income according to date range
-    Route::GET('/expenses/{date_range}', [ExpenseController::class, 'index'])->name('expenses.index');                          // Get all Expense according to date range
-    Route::GET('/accounts/withdrawals/{date_range}', [AccountWithdrawalController::class, 'index'])->name('withdrawal.index');  // Get all Account Withdrawal according to date range
-    Route::GET('/accounts/transfers/{date_range}', [AccountTransferController::class, 'index'])->name('transfer.index');        // Get all Account Transfer according to date range
-    Route::GET('/income-categories/active', [IncomeCategoryController::class, 'get_active_categories']);                        // Get all active income Categories
-    Route::GET('/expense-categories/active', [ExpenseCategoryController::class, 'get_active_categories']);                      // Get all active expense Categories
+
+    // Accounts Additional Routes
+    Route::prefix('accounts')->name('accounts.')->group(function () {
+        // Get active data
+        Route::GET('/active', [AccountController::class, 'get_active_accounts'])->name('accounts.active');
+        Route::GET('/incomes/categories/active', [IncomeCategoryController::class, 'get_active_categories'])->name('accounts.incomes.categories.active');
+        Route::GET('/expenses/categories/active', [ExpenseCategoryController::class, 'get_active_categories'])->name('accounts.expenses.categories.active');
+
+        // Index Route with Date Range Parameter
+        Route::GET('/withdrawals/{date_range}', [AccountWithdrawalController::class, 'index'])->name('accounts.withdrawal.index');  // Get all Account Withdrawal according to date range
+        Route::GET('/transfers/{date_range}', [AccountTransferController::class, 'index'])->name('accounts.transfer.index');        // Get all Account Transfer according to date range
+        Route::GET('/incomes/{date_range}', [IncomeController::class, 'index'])->name('accounts.incomes.index');                             // Get all income according to date range
+        Route::GET('/expenses/{date_range}', [ExpenseController::class, 'index'])->name('accounts.expenses.index');                          // Get all Expense according to date range
+        Route::GET('/transactions/{date_range?}/{account_id?}', [AccountController::class, 'get_all_transactions']);                                          // Get all Category Groups
+    });
 
     // Change Status Routes
-    Route::PUT('/users/change-status/{id}', [UserController::class, 'change_status']);
-    Route::PUT('/fields/change-status/{id}', [FieldController::class, 'change_status']);
-    Route::PUT('/centers/change-status/{id}', [CenterController::class, 'change_status']);
-    Route::PUT('/categories/change-status/{id}', [CategoryController::class, 'change_status']);
-    Route::PUT('/accounts/change-status/{id}', [AccountController::class, 'change_status']);
-    Route::PUT('/income-categories/change-status/{id}', [IncomeCategoryController::class, 'change_status']);
-    Route::PUT('/expense-categories/change-status/{id}', [ExpenseCategoryController::class, 'change_status']);
+    Route::PUT('/users/change-status/{id}', [UserController::class, 'change_status'])->name('users.changeStatus');
+    Route::PUT('/fields/change-status/{id}', [FieldController::class, 'change_status'])->name('fields.changeStatus');
+    Route::PUT('/centers/change-status/{id}', [CenterController::class, 'change_status'])->name('centers.changeStatus');
+    Route::PUT('/categories/change-status/{id}', [CategoryController::class, 'change_status'])->name('categories.changeStatus');
+
+    Route::prefix('accounts')->name('accounts.')->group(function () {
+        Route::PUT('/change-status/{id}', [AccountController::class, 'change_status'])->name('accounts.changeStatus');
+        Route::PUT('/incomes/categories/change-status/{id}', [IncomeCategoryController::class, 'change_status'])->name('accounts.incomes.changeStatus');
+        Route::PUT('/expenses/categories/change-status/{id}', [ExpenseCategoryController::class, 'change_status'])->name('accounts.expenses.changeStatus');
+    });
 
     /**
      * -------------------------------------------------------------------------
@@ -140,13 +153,25 @@ Route::group(['middleware' => ['auth:sanctum', 'verified', 'LangCheck', 'activeU
     Route::apiResource('fields', FieldController::class)->except('show');
     Route::apiResource('centers', CenterController::class)->except('show');
     Route::apiResource('categories', CategoryController::class)->except('show');
-    Route::apiResource('accounts', AccountController::class)->except('show');
-    Route::apiResource('income-categories', IncomeCategoryController::class)->except('show');
-    Route::apiResource('expense-categories', ExpenseCategoryController::class)->except('show');
-    Route::apiResource('incomes', IncomeController::class)->except('show');
-    Route::apiResource('expenses', ExpenseController::class)->except('show');
-    Route::apiResource('accounts/withdrawals', AccountWithdrawalController::class)->except('show');
-    Route::apiResource('accounts/transfers', AccountTransferController::class)->only(['index', 'store']);
+
+    // Accounts Routes
+    Route::prefix('accounts')->name('accounts.')->group(function () {
+        Route::apiResource('/', AccountController::class)->except('show');
+        Route::apiResource('withdrawals', AccountWithdrawalController::class)->except('show');
+        Route::apiResource('transfers', AccountTransferController::class)->only(['index', 'store']);
+
+        // Income Routes
+        Route::prefix('incomes')->name('incomes.')->group(function () {
+            Route::apiResource('/', IncomeController::class)->except('show');
+            Route::apiResource('categories', IncomeCategoryController::class)->except('show');
+        });
+
+        // Income Routes
+        Route::prefix('expenses')->name('expenses.')->group(function () {
+            Route::apiResource('/', ExpenseController::class)->except('show');
+            Route::apiResource('categories', ExpenseCategoryController::class)->except('show');
+        });
+    });
 
     /**
      * -------------------------------------------------------------------------
@@ -156,11 +181,16 @@ Route::group(['middleware' => ['auth:sanctum', 'verified', 'LangCheck', 'activeU
      * Here you can see all of the api independent routes and controllers
      */
     // App Config Routes
-    Route::GET('/approvals-config', [AppConfigController::class, 'get_all_approvals']);
-    Route::GET('/categories-config', [CategoryConfigController::class, 'get_all_categories_config']);
-    Route::PUT('/app-settings-update', [AppConfigController::class, 'app_settings_update']);
-    Route::PUT('/approvals-config-update', [AppConfigController::class, 'approvals_update']);
-    Route::PUT('/categories-config-update', [CategoryConfigController::class, 'config_update']);
+    Route::controller(AppConfigController::class)->group(function () {
+        Route::GET('/app-settings', 'index');
+        Route::GET('/approvals-config', 'get_all_approvals');
+        Route::PUT('/app-settings-update', 'app_settings_update');
+        Route::PUT('/approvals-config-update', 'approvals_update');
+    });
+    Route::controller(CategoryConfigController::class)->group(function () {
+        Route::GET('/categories-config', 'get_all_categories_config');
+        Route::PUT('/categories-config-update', 'config_update');
+    });
 
 
     // Temp Routes
