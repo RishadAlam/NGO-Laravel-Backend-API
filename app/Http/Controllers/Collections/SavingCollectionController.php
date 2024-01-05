186b9bf1518e4ed2a7454f593b0bb453c0138c0a
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-01-05 12:29:32 +0600

    feat: Saving Collection Updated

--- app/Http/Controllers/Collections/SavingCollectionController.php
+++ app/Http/Controllers/Collections/SavingCollectionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Collections;
 
+use App\Helpers\Helper;
 use App\Models\AppConfig;
 use App\Models\field\Field;
 use Illuminate\Http\Request;
@@ -10,8 +11,11 @@ use App\Models\accounts\Account;
 use App\Models\category\Category;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\client\SavingAccount;
 use App\Models\Collections\SavingCollection;
+use App\Models\Collections\SavingCollectionActionHistory;
 use App\Http\Requests\collection\SavingCollectionStoreRequest;
+use App\Http\Requests\collection\SavingCollectionUpdateRequest;
 
 class SavingCollectionController extends Controller
 {
@@ -28,29 +32,23 @@ class SavingCollectionController extends Controller
      */
     public function store(SavingCollectionStoreRequest $request)
     {
-        $data = (object) $request->validated();
+        $data           = (object) $request->validated();
         $is_approved    = AppConfig::get_config('saving_collection_approval');
-        $field_map = [
-            'field_id'                  => $data->field_id,
-            'center_id'                 => $data->center_id,
-            'category_id'               => $data->category_id,
-            'saving_account_id'         => $data->saving_account_id,
-            'client_registration_id'    => $data->client_registration_id,
-            'account_id'                => $data->account_id,
-            'acc_no'                    => $data->acc_no,
-            'installment'               => $data->installment,
-            'deposit'                   => $data->deposit,
-            'description'               => $data->description ?? null,
-            'creator_id'                => auth()->id()
-
-        ];
+        $field_map      = self::set_field_map($data, true);
 
         if ($is_approved) {
-            $field_map['is_approved'] = $is_approved;
-            $field_map['approved_by'] = auth()->id();
+            $field_map += [
+                'is_approved' => $is_approved,
+                'approved_by' => auth()->id()
+            ];
 
             DB::transaction(function () use ($field_map, $data) {
                 SavingCollection::create($field_map);
+                SavingAccount::find($data->saving_account_id)
+                    ->incrementEach([
+                        'total_installment' => $data->installment,
+                        'total_deposited'   => $data->deposit,
+                    ]);
                 Account::find($data->account_id)
                     ->increment('total_deposit', $data->deposit);
             });
@@ -62,19 +60,22 @@ class SavingCollectionController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(SavingCollectionUpdateRequest $request, string $id)
     {
-        //
+        $data       = (object) $request->validated();
+        $collection = SavingCollection::find($id);
+        $histData   = self::set_update_hist($data, $collection);
+
+        DB::transaction(
+            function () use ($id, $collection, $data, $histData) {
+                $collection->update(self::set_field_map($data));
+                SavingCollectionActionHistory::create(Helper::setActionHistory('saving_collection_id', $id, 'update', $histData));
+            }
+        );
+
+        return create_response(__('customValidations.client.collection.update'));
     }
 
     /**
@@ -132,5 +133,62 @@ class SavingCollectionController extends Controller
             'success'   => true,
             'data'      => $collections
         ], 200);
+    }
+
+    /**
+     * Set Saving Collection update hist
+     * 
+     * @param object $data
+     * @param object $collection
+     * 
+     * @return array
+     */
+    private static function set_update_hist($data, $collection)
+    {
+        $histData           = [];
+        $fieldsToCompare    = ['installment', 'deposit', 'description'];
+
+        foreach ($fieldsToCompare as $field) {
+            $clientValue    = $collection->{$field} ?? '';
+            $dataValue      = $data->{$field} ?? '';
+            !Helper::areValuesEqual($clientValue, $dataValue) ? $histData[$field] = "<p class='text-danger'>{$clientValue}</p><p class='text-success'>{$dataValue}</p>" : '';
+        }
+
+        return $histData;
+    }
+
+    /**
+     * Set Saving Collection Field Map
+     * 
+     * @param object $data
+     * @param boolean $is_approved
+     * @param integer $creator_id
+     * 
+     * @return array
+     */
+    private static function set_field_map($data, $new_collection = false)
+    {
+        $map = [
+            'installment'               => $data->installment,
+            'deposit'                   => $data->deposit
+        ];
+
+        if (!empty($data->description) && $data->description != 'null') {
+            $map['description'] = $data->description;
+        }
+        if ($new_collection) {
+            $map += [
+                'field_id'                  => $data->field_id,
+                'center_id'                 => $data->center_id,
+                'category_id'               => $data->category_id,
+                'saving_account_id'         => $data->saving_account_id,
+                'client_registration_id'    => $data->client_registration_id,
+                'account_id'                => $data->account_id,
+                'acc_no'                    => $data->acc_no,
+                'creator_id'                => auth()->id()
+            ];
+        }
+
+        return $map;
     }
 }
--- app/Models/Collections/LoanCollectionActionHistory.php
+++ app/Models/Collections/LoanCollectionActionHistory.php
@@ -2,10 +2,62 @@
 
 namespace App\Models\Collections;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Collections\LoanCollection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class LoanCollectionActionHistory extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'loan_collection_id',
+        'author_id',
+        'name',
+        'image_uri',
+        'action_type',
+        'action_details'
+    ];
+
+    /**
+     * Relationship belongs to Loan Collection model
+     *
+     * @return response()
+     */
+    public function LoanCollection()
+    {
+        return $this->belongsTo(LoanCollection::class)->withTrashed();
+    }
+
+    /**
+     * Relationship belongs to User model
+     *
+     * @return response()
+     */
+    public function Author()
+    {
+        return $this->belongsTo(User::class, 'author_id', 'id')->withTrashed();
+    }
+
+    /**
+     * Mutator for action Details json Data
+     */
+    public function setActionDetailsAttribute($value)
+    {
+        $this->attributes['action_details'] = json_encode($value);
+    }
+
+    /**
+     * accessor for action Details json Data
+     */
+    public function getActionDetailsAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
