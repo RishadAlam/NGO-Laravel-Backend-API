5718b2ace2b50e7983681c87889342462dea6338
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2025-07-27 22:29:14 +0600

    feat:

---
+++ database/migrations/2025_07_27_211928_create_savings_to_savings_transactions.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('savings_to_savings_transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('creator_id')->constrained('users')->cascadeOnUpdate('cascade')->cascadeOnDelete('cascade');
+            $table->foreignId('approved_by')->nullable()->constrained('users')->cascadeOnUpdate('cascade')->nullOnDelete();
+
+            $table->foreignId('tx_acc_id')->constrained('saving_accounts', 'id')->cascadeOnUpdate('cascade')->cascadeOnDelete('cascade')->comment('Transaction Sended Account');
+            $table->foreignId('rx_acc_id')->constrained('saving_accounts', 'id')->cascadeOnUpdate('cascade')->cascadeOnDelete('cascade')->comment('Transaction Received Account');
+
+            $table->integer('amount');
+
+            $table->integer('tx_prev_balance');
+            $table->integer('tx_balance')->storedAs('tx_prev_balance - amount');
+
+            $table->integer('rx_prev_balance');
+            $table->integer('rx_balance')->storedAs('rx_prev_balance + amount');
+
+            $table->mediumText('description')->nullable();
+
+            $table->boolean('is_approved')->default(false);
+
+            $table->timestamp('approved_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('savings_to_savings_transactions');
+    }
+};
